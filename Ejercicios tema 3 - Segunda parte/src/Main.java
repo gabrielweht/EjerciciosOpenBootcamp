2df88c8c2baa00d5c545439cc3040c631906ed5e
gabrielweht <gabrielestebanw@gmail.com>
2023-02-17 15:34:45 -0300

    Change sumaPuertas

--- Ejercicios tema 3 - Segunda parte/src/Main.java
+++ Ejercicios tema 3 - Segunda parte/src/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Coche miCoche = new Coche();
         miCoche.sumaPuertas();
-        miCoche.sumaPuertas();
-        miCoche.sumaPuertas();
-        miCoche.sumaPuertas();
         System.out.println("Mi coche tiene " + miCoche.puertas + " puertas");
     }
 }
