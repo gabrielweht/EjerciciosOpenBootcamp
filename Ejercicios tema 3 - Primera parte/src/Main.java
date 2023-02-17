395d349fa3121a843d71cf3dc7a603db8f832982
gabrielweht <gabrielestebanw@gmail.com>
2023-02-17 15:57:54 -0300

    Add file primera parte

---
+++ Ejercicios tema 3 - Primera parte/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = sumaParam(51, 45, 84);
+        System.out.println(resultado);
+    }
+
+    public static int sumaParam(int a, int b, int c){
+        return a + b + c;
+    }
+}
+
