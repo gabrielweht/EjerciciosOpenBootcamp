b7d92f7fb6c4e569d216b37206e7eb147c139af5
gabrielweht <gabrielestebanw@gmail.com>
2023-03-01 15:35:33 -0300

    Ejercicios tema 4 - Primer punto

---
+++ Ejercicios tema 4 - Primer punto/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroIf = 0;
+        if (numeroIf < 0){
+            System.out.println("numeroIf es negativo");
+        } else if (numeroIf > 0) {
+            System.out.println("numeroIf es positivo");
+        } else {
+            System.out.println("numeroIf es cero");
+        }
+    }
+}
